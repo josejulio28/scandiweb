2a99cccbdcadc56b1793a8882bc8b426187a37ab
jose ferreira <zejulio3@hotmail.com>
2022-04-30 17:53:14 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import navbar from "./components/navbar/navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class navbar extends React.Component {
+  render() {
+    return (
+      <>
+        <a>WOMEN</a>
+        <a>MEN</a>
+        <a>KIDS</a>
+        <a>KIDS</a>
+      </>
+    );
+  }
+}
+
+export default navbar;
